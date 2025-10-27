fd54470a0d0c118846f450ac6aa3461a232eba0f
lgabelashvili <gabelashvili1999@gmail.com>
2025-10-27 20:37:29 +0400

    refactor: comment out LangDropdown and NotificationDropdown in AppHeader component

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -172,11 +172,11 @@ const AppHeader: React.FC = () => {
           } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
-            <LangDropdown />
+            {/* <LangDropdown /> */}
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
-            <NotificationDropdown />
+            {/* <NotificationDropdown /> */}
             {/* <!-- Notification Menu Area --> */}
           </div>
           {/* <!-- User Area --> */}
